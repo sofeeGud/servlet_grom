b9aa21a734a07abb500fb79e9f1310a701d15361
Lenovo T540p <sonychkalapochka94@gmail.com>
2020-04-04 21:08:43 +0300

    servlet les2

---
+++ src/main/java/lesson2/model/Item.java
@@ -0,0 +1,72 @@
+package lesson2.model;
+
+import javax.persistence.*;
+import java.util.Date;
+@Entity
+@Table(name = "ITEM")
+public class Item {
+    private  long id;
+    private String name;
+    private Date dateCreated;
+    private Date lastUpdatedDate;
+    private String description;
+
+    @Id
+    @SequenceGenerator(name = "ITEM_SEQ", sequenceName = "ITEM_SEQ", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ITEM_SEQ")
+    @Column(name = "ID")
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Column(name = "NAME")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "DATE_CREATED")
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    @Column(name = "LAST_UPDATE_DATE")
+    public Date getLastUpdatedDate() {
+        return lastUpdatedDate;
+    }
+
+    public void setLastUpdatedDate(Date lastUpdatedDate) {
+        this.lastUpdatedDate = lastUpdatedDate;
+    }
+
+    @Column(name = "DESCRIPTION")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", dateCreated=" + dateCreated +
+                ", lastUpdatedDate=" + lastUpdatedDate +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
